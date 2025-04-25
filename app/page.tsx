ab63792e2258240a7dfa4b4e0a72067e61dfc4f5
Marton Tasnadi <marton.tasnadi@gmail.com>
2025-04-25 23:55:49 +0200

    add page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
 export default function Home() {
-  return <div>kistasi.hu</div>;
+  return (
+    <div>
+      <div className="bg-[#f2f8fa] h-screen text-[#2c3144] text-center pt-20">
+        <div className="text-3xl font-bold italic mb-8">
+          Hi, I&apos;m <b>kistasi</b>.
+        </div>
+
+        <div className="text-xl">
+          Please check out my{" "}
+          <a
+            className="underline text-blue-700"
+            href="https://www.linkedin.com/in/kistasi/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn profile
+          </a>
+          ! :)
+        </div>
+      </div>
+    </div>
+  );
 }
